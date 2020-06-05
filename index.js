62074e3374a8fba9c83758e04baa46d40fcdb2b7
Jessica <github email address>
2020-06-04 20:09:48 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
 // Write your code here
+
+const num1 = 2
+const num2 = 31
+
+const multiply = num1 * num2
+
+const random = (Math.random() * 10)
+
+const mod = 14 % 5
+
+const max = Math.max(1, 2, 3, 4, 5, 20)
